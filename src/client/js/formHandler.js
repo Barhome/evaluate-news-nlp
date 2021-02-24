c81c72a28fdd834aecb729f214a7bfe752b382f0
barhome <barhome@github.com>
2021-02-24 04:26:12 +0200

    test:Trying to chain promises

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -9,6 +9,17 @@ const postUserUrlData = async function (url = "", data = {}) {
   });
 };
 
+// create function to Get Data from server
+const getServerData = async function (url = "") {
+  const response = await fetch(url);
+  try {
+    const dataServer = response.json();
+    return dataServer;
+  } catch (error) {
+    console.log(`error:${error}`);
+  }
+};
+
 function handleSubmit(event) {
   event.preventDefault();
   // selecting dom elements
@@ -22,50 +33,36 @@ function handleSubmit(event) {
   }
 
   // call the function to post data to server
-
-  postUserUrlData("http://localhost:3000/postUserUrl", {
-    userUrl: urlAddress,
-  });
-
-  // fetching data from the server and updating ui
-
-  fetch("http://localhost:3000/getServerEndPoint")
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data.score_tag);
-      if (data.score_tag == "P+")
-        document.getElementById("results").innerHTML =
-          "The review tone is strong positive";
-      else if (data.score_tag == "P")
-        document.getElementById("results").innerHTML =
-          "The review tone is  positive";
-      else if (data.score_tag == "NEU")
-        document.getElementById("results").innerHTML =
-          "The review tone is neutral";
-      else if (data.score_tag == "N")
-        document.getElementById("results").innerHTML =
-          "The review tone is negative";
-      else if (data.score_tag == "N+")
-        document.getElementById("results").innerHTML =
-          "The review tone is strong negative";
-      else if (data.score_tag == "NONE")
-        document.getElementById("results").innerHTML =
-          "There is no review tone";
-      else
-        document.getElementById("results").innerHTML =
-          "Failed to show review due to system error";
-    });
-
-  // // check what text was put into the form field
-  // let formText = document.getElementById('name').value
-  // checkForName(formText)
-
-  // console.log("::: Form Submitted :::")
-  // fetch('http://localhost:3000/test')
-  // .then(res => res.json())
-  // .then(function(res) {
-  //     document.getElementById('results').innerHTML = res.message
-  // })
+  else {
+    postUserUrlData("http://localhost:3000/postUserUrl", {
+      userUrl: urlAddress,
+    })
+      .then(() => getServerData("http://localhost:3000/getServerEndPoint"))
+      .then((data) => {
+        console.log(data);
+        if (data.score_tag == "P+")
+          document.getElementById("results").innerHTML =
+            "The review tone is strong positive";
+        else if (data.score_tag == "P")
+          document.getElementById("results").innerHTML =
+            "The review tone is  positive";
+        else if (data.score_tag == "NEU")
+          document.getElementById("results").innerHTML =
+            "The review tone is neutral";
+        else if (data.score_tag == "N")
+          document.getElementById("results").innerHTML =
+            "The review tone is negative";
+        else if (data.score_tag == "N+")
+          document.getElementById("results").innerHTML =
+            "The review tone is strong negative";
+        else if (data.score_tag == "NONE")
+          document.getElementById("results").innerHTML =
+            "There is no review tone";
+        // else
+        //   document.getElementById("results").innerHTML =
+        //     "Failed to show review due to system error";
+      })
+      .catch((error) => console.log(error));
+  }
 }
-
 export { handleSubmit };
